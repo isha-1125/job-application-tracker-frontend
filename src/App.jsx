b3c3a3eaca9000ac59bf5118f14e3779152b950c
isha-1125 <ishabaghele7@gmail.com>
2026-02-15 23:55:18 -0800

    Added signup and fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-
 
 export default function App() {
   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
@@ -10,11 +10,17 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Protected Route */}
         <Route
           path="/dashboard"
           element={userInfo ? <Dashboard /> : <Navigate to="/login" />}
         />
+
+        {/* Default Redirect */}
         <Route
           path="*"
           element={<Navigate to={userInfo ? "/dashboard" : "/login"} />}
